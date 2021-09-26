29b0377c17b87e8c643ea92d527a28f6003df5f7
Md. Imriaz Uddin <imriaz.cse.bubt@gmail.com>
2021-09-26 22:10:39 +0600

    Assignment 8

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h2 className="header-title">Capston projects group</h2>
+            <p>Capston project is a final year project for CSE student.It is a combine course for thesis and project together.</p>
+            <h3>Total Weiver Budget for wach group: 50000</h3>
+        </div>
+    );
+};
+
+export default Header;
